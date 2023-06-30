6c23d990c86bdbbfb0f8b3fa9d3572de9069c4e7
benforster12310 <40578406+benforster12310@users.noreply.github.com>
2023-06-30 09:32:42 +0100

    Update SecondaryProcess.ino

--- SecondaryProcess/SecondaryProcess.ino
+++ SecondaryProcess/SecondaryProcess.ino
@@ -1,1 +1,20 @@
+// BlockSignallingV4 Secondary Process
+
+// This program takes instructions and executes them
+
+// Define some things
+#include "ArduinoJson.h"
+
+// then define the pin to subtract
+// this defines the conversion between the virtual pins on the primary board
+// and the actial pins on this secondary board
+int pinToSubtract = 100;
+
+void setup() {
+    // then setup the debugging serial port
+	Serial.begin(9600);
+	// then setup the board communication port 
+	Serial1.begin(115200);
+	
+}
 
